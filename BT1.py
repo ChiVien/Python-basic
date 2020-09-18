61983f1185d0098e2d2a507cfb16518d21f691f4
ChiVien <56378777+ChiVien@users.noreply.github.com>
2020-09-18 22:44:52 +0700

    Add files via upload

---
+++ BT1.py
@@ -0,0 +1,68 @@
+
+#B1
+a,b=5,17
+S=a*a+b*b
+print(S)
+#B2
+n=float(input("Nhap Tien Luong: "))
+m=float(input("Nhap Tien Phu Cap: "))
+k=int(input("Nhap So Ngay Lam: "))
+P=((n+m)/22)*k
+print("Tien Luong Chinh: ",P)
+#B3
+r=float(input("Nhap Ban Kinh: "))
+Cvi=r*2*3.14
+DT=r*r*3.14
+print("Chu Vi: ",Cvi)
+print("Dien Tich: ",DT)
+#B4
+l=int(input("Nhap So Nguyen: "))
+hch=l/10
+hdv=l%10
+print("Hang Chuc: ",hch)
+print("Hang Don Vi: ",hdv)
+#B5
+HT="Cao Dương Chí Viễn"
+type(HT)
+
+
+#B4
+print("Hello Python")
+#B5
+print("Cao Dương Chí Viễn")
+#B6
+print("Cao Dương Chí Viễn")
+print("1906020140")
+print("CNTT")
+print("13THC")
+print("Lập Trình Ứng Dụng")
+#B7
+n1=int(input("Số Nguyên Thứ 1: "))
+n2=int(input("Số Nguyên Thứ 2: "))
+R=n1+n2
+print(n1,"+",n2,"=",R)
+#B8
+print("********")
+print("********") 
+print("********")  
+print("********")
+#B9
+print("********")
+print("*      *") 
+print("*      *")  
+print("********")
+#B10
+n = 5
+for i in range(1, n):
+    s = n - i
+    k = s * " "
+    print(k, end='')
+    print(((2 * i) - 1) * '*')
+#B11
+cd=int(input("Nhap cd:"))
+cr=int(input("Nhap cr:"))
+for i in range(cr):
+    for j in range (cd):
+        print("*",end='')
+    print()
+#B12
